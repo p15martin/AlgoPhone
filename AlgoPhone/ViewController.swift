05191937ff0f4cc3e039a100ace08902a390e867
Peter Martin <p15martin@outlook.com>
2015-03-30 16:24:29 -0700

    Initial version

--- AlgoPhone/ViewController.swift
+++ AlgoPhone/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import algorithmia
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        algorithmia.callalgorithmia()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome!
+
+This an iOS app which demonstrates use of the [algorithmia](https://github.com/p15martin/algorithmia) library. Before using it you'll need to fetch and build [algorithmia](https://github.com/p15martin/algorithmia)...
+
+Make sure they share the same parent folder!
